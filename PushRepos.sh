d5df7ae3a1fcf6697ca24258c1d9ebcc82d6f8af
Miguel Ayala <71942809+mi-ayala@users.noreply.github.com>
2022-01-12 01:39:43 -0500

    Create PushRepos.sh

---
+++ PushRepos.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  ==================================================
+#  PushRepos.sh
+#  ==================================================
+#  DESCRIPTION 
+#  Update all git repos in current folder at once.
+#  __________________________________________________
+#  Miguel Ayala, 06-Jan-2022.
+#  ==================================================
+
+for d in */ ; do
+    
+    [ -L "${d%/}" ] && continue
+    
+    echo "$d"
+    cd $d
+
+    if [ -d ".git" ] ;
+      then   
+      git pull -v
+    fi;
+
+    cd ..
+
+done;
